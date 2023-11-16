6c861e077c71b6f76ce45c3976a5c93841cac9a7
andalysaid <aly.said@and.digital>
2023-11-16 17:03:59 +0000

    stuf

--- sandbox-example/src/App.js
+++ sandbox-example/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React to get money
+          Learn React to get money and stuff
         </a>
         <btn href="www.google.com">Click NOW PLEASE.</btn>
       </header>
